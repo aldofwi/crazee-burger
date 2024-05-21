0b4107a49e9493baa5ed0b4a3649b810678ef46c
Aldo FWI <aldo.fwi@gmail.com>
2024-05-21 15:44:15 -0400

    F01

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,15 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+    const [name, setName] = useState("");
+
+    const handleName = (e) => {
+      e.preventDefault();
+
+      alert("Bonjour "+name);
+      setName("");
+    }
 
   return (
     <>
@@ -16,20 +24,33 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Count is {count}
-        </button>
-        <p>
-          Hi Aldo!
+      <h1>Bienvenue chez nous !</h1>
+
+      <form id="card" onSubmit={handleName}>
+
+        <p className="read-the-docs">
+          <label>Connectez-vous</label>
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <br></br>
+
+        <input 
+          required
+          id="name"
+          name="name" 
+          type="text"
+          value={name || ""}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Entrez votre prénom.." />
+          <br></br>
+
+        <input 
+          type="submit"
+          value="Accéder à votre espace" />
+
+      </form>
+
     </>
   )
 }
 
-export default App
+export default App;
