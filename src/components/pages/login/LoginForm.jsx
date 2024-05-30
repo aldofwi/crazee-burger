abc36bb8d2128d9171474e32a77889da679ae2fb
Aldo FWI <aldo.fwi@gmail.com>
2024-05-30 01:43:33 -0400

    💄 F03 : LoginForm > Button > Add Styling

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,6 +1,7 @@
 import { useState, React} from 'react'
 import { useNavigate } from 'react-router-dom';
 import { BsPersonCircle } from "react-icons/bs";
+import { IoChevronForward } from "react-icons/io5";
 import { styled } from 'styled-components';
 import { theme } from '../theme';
 
@@ -40,7 +41,10 @@ export default function LoginForm() {
               placeholder="Entrez votre prénom" />
           </div>
             
-          <button type="submit">Accéder à mon espace {'>'}</button>
+          <button className="button-with-icon">
+            <span type="submit">Accéder à mon espace</span>
+            <IoChevronForward className="icon" />
+          </button>
         </div>
 
     </LoginFormStyled>
@@ -94,6 +98,7 @@ const LoginFormStyled = styled.div`
       border: none;
       font-size: 15px;
       color: #17161a;
+      width: 100%;
     }
 
     &::placeholder {
@@ -101,6 +106,54 @@ const LoginFormStyled = styled.div`
       color: lightgrey;
     }
   }
+
+  .button-with-icon {
+    width: 100%;
+    border: 1px solid red;
+    display: inline-flex;
+    justify-content : center;
+    align-items: center;
+    position: relative;     // in case we create interactive elements
+    white-space: nowrap;    // Prevent the text label from wrapping
+    text-decoration: none;  // Remove the text decoration
+    line-height: 1;
+
+    padding: 18px 24px;
+    border-radius: 5px;
+    font-size: 15px;
+    font-weight: 800;
+    color: white;
+    background-color: #ff9f1b;
+    border: 1px solid #ff9f1b;
+  
+      &:hover:not(:disabled) {
+        background-color: white;
+        color: #ff9f1b;
+        border: 1px solid #ff9f1b;
+        transition: all 200ms ease-out;
+      }
+
+      &:active {
+        color: white;
+        background-color: #ff9f1b;
+        border: 1px solid #ff9f1b;
+      }
+
+      &:disabled {
+        opacity: 0.6;
+        cursor: not-allowed;
+      }
+
+      .icon {
+        display: flex;
+        justify-content : center;
+        align-items: center;
+        font-size: 15px;
+        margin-left: 10px;
+      }
+
+    }
+
 
 
   /* h1 {
