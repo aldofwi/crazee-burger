1675325f8425a52ad9288016419346aefbe55f6e
Aldo FWI <aldo.fwi@gmail.com>
2024-05-30 16:38:24 -0400

    ♻️ F03 : LOGIN Input > Create reusable <Input />

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,9 +1,10 @@
 import { useState, React} from 'react'
 import { useNavigate } from 'react-router-dom';
-import { BsPersonCircle } from "react-icons/bs";
 import { IoChevronForward } from "react-icons/io5";
 import { styled } from 'styled-components';
 import { theme } from '../theme';
+import Input from './Input';
+import { BsPersonCircle } from 'react-icons/bs';
 
 
 export default function LoginForm() {
@@ -11,16 +12,20 @@ export default function LoginForm() {
     const [name, setName] = useState("");
     const navigate = useNavigate();
 
+    const handleSubmit = () => { 
+      navigate(`order/${name}`);
+      setName("");
+     }
+
     const handleName = (e) => {
         e.preventDefault();
 
-        setName("");
-        navigate(`order/${name}`);
+        setName(e.target.value); 
     }
 
   return (
     
-    <LoginFormStyled onSubmit={handleName}>
+    <LoginFormStyled action="submit" onSubmit={handleSubmit}>
 
         <h1>Bienvenue chez nous !</h1>
         <hr />
@@ -29,17 +34,13 @@ export default function LoginForm() {
         <br />
         
         <div>
-          <div className="input-with-icon">
-            <BsPersonCircle className="icon" />
-            <input
-              required
-              id="name"
-              name="name"
-              type="text"
-              value={name || ""}
-              onChange={(e) => setName(e.target.value)}
-              placeholder="Entrez votre prénom" />
-          </div>
+          <Input
+            value={name}
+            onChange={handleName}
+            placeholder={"Entrez votre prénom"}
+            iconBeforeInput={<BsPersonCircle className="icon" />}
+            required
+          />
             
           <button className="button-with-icon">
             <span type="submit">Accéder à mon espace</span>
@@ -77,33 +78,6 @@ const LoginFormStyled = styled.form`
     margin: 20px 10px 10px;
     color: white;
     font-size: 36px;
-  }
-
-  .input-with-icon {
-    background-color: #fff;
-    border-radius: 5px;
-    display: flex;
-    align-items: center;
-    padding: 18px 24px;
-    margin: 18px 0;
-
-    .icon {
-      font-size: 15px;
-      margin-right: 8px;
-      color: #93a2b1;
-    }
-
-    input {
-      border: none;
-      font-size: 15px;
-      color: #17161a;
-      width: 100%;
-    }
-
-    &::placeholder {
-      background: white;
-      color: lightgrey;
-    }
   }
 
   .button-with-icon {
