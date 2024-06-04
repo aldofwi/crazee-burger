b199967edef77570e02bce38c818d0d45ada042e
Aldo FWI <aldo.fwi@gmail.com>
2024-06-04 18:36:21 -0400

    🚧 F05 : ORDER > MENU > Add Grid Layout

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -19,14 +19,16 @@ const MainStyled = styled.div`
 
   background: ${theme.colors.background_white};
   flex: 1; // height: calc(95vh - 10vh);
-  
+
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
   //border-radius: 0px 0px 15px 15px;
 
   display: grid;
-  grid-template-columns: 1fr ;
+  grid-template-columns: 1fr;
+
+  overflow-y: scroll;
 
   /* 
   <div class="basket">Basket</div>
--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -12,15 +12,9 @@ export default function Menu() {
 
     <MenuStyled className="menu">
 
-        Menu
-
-        {menu.map((item, i) => {
+        {menu.map((item) => {
             return (
-                <ProductCard
-                    image={item.imageSource}
-                    titre={item.title}
-                    prix={formatPrice(item.price)}
-                />
+                <div class="produit">{item.title}</div>
             )
         })}
     
@@ -32,13 +26,26 @@ export default function Menu() {
 const MenuStyled = styled.div`
 
     background: purple;
-    padding: 50px 50px 150px;
+    display: grid;
     grid-row-gap: 60px;
+    grid-template-columns: repeat(4, 1fr);
+    padding: 50px 50px 150px;
+    justify-items: center;
+
+    .produit {
+        background: red;
+        width: 240px;
+        height: 330px;
+    }
 
 `
 
 /*
 
-
+<ProductCard className="produit"
+    image={item.imageSource}
+    titre={item.title}
+    prix={formatPrice(item.price)}
+/>
 
 */
