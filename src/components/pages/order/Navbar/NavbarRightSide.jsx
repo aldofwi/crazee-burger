d2635921fb1efaeb4fb215355e57184bd841b796
Aldo FWI <aldo.fwi@gmail.com>
2024-06-05 21:03:03 -0400

    F06 : NAVBAR > Add <ToastAdmin/>

--- src/components/pages/order/Navbar/NavbarRightSide.jsx
+++ src/components/pages/order/Navbar/NavbarRightSide.jsx
@@ -1,10 +1,10 @@
 import ToggleButton from "../../../reusable-ui/ToggleButton"
-import { toast, ToastContainer } from "react-toastify";
+import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css"
 import styled from "styled-components"
 import Profile from "./Profile"
-import { theme } from "../../theme";
 import { useState } from "react";
+import ToastAdmin from "./ToastAdmin";
 
 export default function NavbarRightSide({ username }) {
 
@@ -40,10 +40,7 @@ export default function NavbarRightSide({ username }) {
           labelIfChecked={"Désactiver le mode admin"}
           labelIfUnchecked={"Activer le mode admin"}
         />
-        <ToastContainer
-          className="toaster"
-          bodyClassName="body-toast"
-        />
+        <ToastAdmin />
         
         <Profile username={username} />
 
@@ -71,24 +68,6 @@ const NavbarRightSideStyled = styled.nav`
 
   .profile {
     background: yellow;
-  }
-
-  .toaster {
-    max-width: 300px;
-  }
-
-  .Toastify__toast.Toastify__toast-theme--dark.Toastify__toast--info {
-    background: ${theme.colors.background_dark};
-  }
-
-  .body-toast {
-    .Toastify__toast-icon.Toastify--animate-icon.Toastify__zoom-enter {
-      margin-right: 20px;
-      margin-left: 5px;
-    }
-    div {
-      line-height: 1.3em;
-    }
   }
 
 `
