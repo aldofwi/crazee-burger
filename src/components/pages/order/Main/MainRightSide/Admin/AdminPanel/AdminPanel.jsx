106a84faf5f7ec4df2e61da46118337230ac858d
Aldo FWI <aldo.fwi@gmail.com>
2024-06-18 20:36:54 -0400

    💄F08: ADMIN > Panel > Styling Panel

--- src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/AdminPanel.jsx
+++ src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/AdminPanel.jsx
@@ -24,10 +24,11 @@ export default function AdminPanel() {
 
 const AdminPanelStyled = styled.div`
 
-  height: 250px;
+  height: 240px;
   background: ${theme.colors.white};
   border: 1px solid ${theme.colors.greyLight};
   box-shadow: ${theme.shadows.subtle};
+  box-sizing: border-box;
   padding: 30px 5%; // 30px en haut & en bas // 5% gauche droite
 
 `
