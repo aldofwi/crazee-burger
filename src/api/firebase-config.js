8ee34f92461322c21def70edf65e28bf78718f6b
Aldo FWI <aldo.fwi@gmail.com>
2024-07-26 15:37:33 -0400

    📦 F13 : DATABASE > Add Firebase v10

---
+++ src/api/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDJ5PaTL3gcCKxqoeVxRDqol-Fimv3MpA0",
+  authDomain: "crazee-burger-2faa8.firebaseapp.com",
+  projectId: "crazee-burger-2faa8",
+  storageBucket: "crazee-burger-2faa8.appspot.com",
+  messagingSenderId: "759019864117",
+  appId: "1:759019864117:web:a170e6fa58d93745c5f17c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
